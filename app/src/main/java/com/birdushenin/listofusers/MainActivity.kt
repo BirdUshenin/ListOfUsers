13d590071e40fea583acedf44778354a992e6a0d
Ilya <ilyaushenin@gmail.com>
2023-11-14 16:43:28 +0300

    Fragment

--- app/src/main/java/com/birdushenin/listofusers/MainActivity.kt
+++ app/src/main/java/com/birdushenin/listofusers/MainActivity.kt
@@ -3,39 +3,27 @@ package com.birdushenin.listofusers
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View.VISIBLE
+import androidx.fragment.app.FragmentContainerView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.birdushenin.listofusers.DataUsers.userServer
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var adapter: AdapterUsers
-    private lateinit var fragmentEdit: EditUserFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
 
-        val userServer = DataUsers.userServer
-        adapter = AdapterUsers(userServer)
+        val fragmentUser = UserList()
+        val fragmentManager = supportFragmentManager
 
-        fragmentEdit = EditUserFragment()
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.fragUsers, fragmentUser)
+        transaction.addToBackStack("FragmentUser")
+        transaction.commit()
 
-        adapter.setOnUserItemClickListener(object: OnUserItemClickListener {
-            override fun onUserItemClicked(user: Users) {
-                val transaction = supportFragmentManager.beginTransaction()
-//                val args = Bundle()
-//                args.putSerializable("user", user)
-//                fragmentEdit.arguments = args
-                transaction.replace(R.id.fragEdit, fragmentEdit)
-                transaction.addToBackStack("FragmentEdit")
-                transaction.commit()
-            }
-        })
-
-        recyclerView.adapter = adapter
     }
 }
