8821c57f6d49f9e01c444bbb707486e8980d53e9
Ilya <ilyaushenin@gmail.com>
2023-11-14 18:32:27 +0300

    FragmentEdit

--- app/src/main/java/com/birdushenin/listofusers/DataUsers.kt
+++ app/src/main/java/com/birdushenin/listofusers/DataUsers.kt
@@ -1,11 +1,11 @@
 package com.birdushenin.listofusers
 
 object DataUsers {
-    val userServer = listOf(
-        Users("https://th.bing.com/th/id/R.5b09ef24914044ec1fa7e3e81b95c8b8?rik=the%2fiOzVxcm%2bFQ&riu=http%3a%2f%2f1.bp.blogspot.com%2f-i0ZgN3VaooM%2fU3G2t2unHYI%2fAAAAAAAAdzA%2fhc_T6HtxJko%2fs1600%2f005.png&ehk=VJt3MiJEhAPgZ5EmVYbSV5FcdhZgRr5T9VMxbXk%2fVV8%3d&risl=&pid=ImgRaw&r=0", "Гомэр", "Симпсон", "+132 244 242"),
-        Users("https://th.bing.com/th/id/R.06dcea4733cfb34c3aa5bac93025af47?rik=cjc6xTIyoFVMqA&riu=http%3a%2f%2fwww.simpsons-art.ru%2fpersonas%2fbart.png&ehk=T4RJlX49OveOKPwCfZ3hlelpc005pKaBzat2Im7q42c%3d&risl=&pid=ImgRaw&r=0", "Барт", "Симпсон", "+122 344 745"),
-        Users("https://th.bing.com/th/id/R.579f11e76859206a36900fdc88269116?rik=xEVDERhAT2wLkQ&riu=http%3a%2f%2falfaday.net%2fuploads%2fposts%2f2013-03%2f1364723291_1gbucoltrp7ez2b.jpg&ehk=scRxIbRckZRQvqL9Vcm9%2bTFpOOHSJb%2bAk6bDIiEXj9c%3d&risl=&pid=ImgRaw&r=0", "Лиза", "Симпсон", "+192 144 512"),
-        Users("https://th.bing.com/th/id/OIP._JNFEl1YC7FlenFWj5kxhgD6D6?pid=ImgDet&rs=1", "Ральф", "Виггум", "+198 123 894"),
-        Users("https://th.bing.com/th/id/R.a32fa27bf2b8ea6013e11e6f73c763ea?rik=p4aEan24DguwLg&riu=http%3a%2f%2fmila.kcbux.ru%2fRaznoe%2fIskusstvo%2fKino%2fSimpson%2fimage%2fsim_oboi_022.jpg&ehk=XAtz702lU4Si3BmBgeI4RqcyR74%2fUstvmNs6ilOc8Rc%3d&risl=&pid=ImgRaw&r=0", "Вилл", "Раус", "+153 411 124")
+    val userServer = mutableListOf(
+        Users(1,"https://th.bing.com/th/id/R.5b09ef24914044ec1fa7e3e81b95c8b8?rik=the%2fiOzVxcm%2bFQ&riu=http%3a%2f%2f1.bp.blogspot.com%2f-i0ZgN3VaooM%2fU3G2t2unHYI%2fAAAAAAAAdzA%2fhc_T6HtxJko%2fs1600%2f005.png&ehk=VJt3MiJEhAPgZ5EmVYbSV5FcdhZgRr5T9VMxbXk%2fVV8%3d&risl=&pid=ImgRaw&r=0", "Гомэр", "Симпсон", "+132 244 242"),
+        Users(2, "https://th.bing.com/th/id/R.06dcea4733cfb34c3aa5bac93025af47?rik=cjc6xTIyoFVMqA&riu=http%3a%2f%2fwww.simpsons-art.ru%2fpersonas%2fbart.png&ehk=T4RJlX49OveOKPwCfZ3hlelpc005pKaBzat2Im7q42c%3d&risl=&pid=ImgRaw&r=0", "Барт", "Симпсон", "+122 344 745"),
+        Users(3, "https://th.bing.com/th/id/R.579f11e76859206a36900fdc88269116?rik=xEVDERhAT2wLkQ&riu=http%3a%2f%2falfaday.net%2fuploads%2fposts%2f2013-03%2f1364723291_1gbucoltrp7ez2b.jpg&ehk=scRxIbRckZRQvqL9Vcm9%2bTFpOOHSJb%2bAk6bDIiEXj9c%3d&risl=&pid=ImgRaw&r=0", "Лиза", "Симпсон", "+192 144 512"),
+        Users(4, "https://th.bing.com/th/id/OIP._JNFEl1YC7FlenFWj5kxhgD6D6?pid=ImgDet&rs=1", "Ральф", "Виггум", "+198 123 894"),
+        Users(5, "https://th.bing.com/th/id/R.a32fa27bf2b8ea6013e11e6f73c763ea?rik=p4aEan24DguwLg&riu=http%3a%2f%2fmila.kcbux.ru%2fRaznoe%2fIskusstvo%2fKino%2fSimpson%2fimage%2fsim_oboi_022.jpg&ehk=XAtz702lU4Si3BmBgeI4RqcyR74%2fUstvmNs6ilOc8Rc%3d&risl=&pid=ImgRaw&r=0", "Вилл", "Раус", "+153 411 124")
     )
 }
--- app/src/main/java/com/birdushenin/listofusers/UserList.kt
+++ app/src/main/java/com/birdushenin/listofusers/UserList.kt
@@ -1,26 +1,23 @@
 package com.birdushenin.listofusers
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
-import android.view.View.VISIBLE
 import android.view.ViewGroup
-import androidx.fragment.app.FragmentContainerView
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.birdushenin.listofusers.databinding.FragmentUserListBinding
 
-class UserList : Fragment() {
+class UserList : Fragment(), OnUserEditListener {
 
     private lateinit var adapter: AdapterUsers
 
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         val userServer = DataUsers.userServer
         adapter = AdapterUsers(userServer)
 
@@ -32,14 +29,25 @@ class UserList : Fragment() {
             adapter.setOnUserItemClickListener(object: OnUserItemClickListener {
             override fun onUserItemClicked(user: Users) {
                 val fragmentB = EditUserFragment()
+                fragmentB.setOnUserEditListener(this@UserList)
+                fragmentB.setUser(user)
+
                 requireActivity().supportFragmentManager.beginTransaction()
                     .replace(R.id.fragEdit, fragmentB)
                     .addToBackStack("FragmentB")
                     .commit()
             }
         })
-
         recyclerView.adapter = adapter
         return binding.root
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onUserEdited(user: Users) {
+        val index = DataUsers.userServer.indexOfFirst { it.id == user.id }
+        if (index != -1) {
+            DataUsers.userServer[index] = user
+            adapter.notifyDataSetChanged()
+        }
     }
 }
